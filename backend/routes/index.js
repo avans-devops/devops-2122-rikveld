f395253d22deb551232278dc3f5f185720f54e71
RikVeld <rhcveld@avans.nl>
2022-04-08 14:23:31 +0200

    redo week 1

--- backend/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router(); //eslint-disable-line
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
