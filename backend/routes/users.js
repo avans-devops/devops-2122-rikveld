3561614df6ba1ac6aa165a550da2b2c34a09280f
RikVeld <rhcveld@avans.nl>
2022-06-21 13:23:56 +0200

    Update users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -32,10 +32,4 @@ router.route('/').get((req, res) => {
       });
     });
 
-router.get('/slow', function(re, res, next) {
-  setTimeout(() => {
-    res.json({foo: 'bar'})
-  }, 3000)
-});
-
 module.exports = router;
